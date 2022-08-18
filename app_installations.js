9460eb59208c6b5998b7e6257563435e354f2a36
mondragon-ai <angel.mondragon@yahoo.com>
2022-08-18 11:05:33 -0500

    deploy heroku

--- app_installations.js
+++ app_installations.js
@@ -4,12 +4,6 @@ export const AppInstallations = {
   includes: async function (shopDomain) {
     const shopSessions = await Shopify.Context.SESSION_STORAGE.findSessionsByShop(shopDomain);
 
-    const s = shopSessions[0]
-
-    console.log("7: - App install.js -  SHop Session -- \n", typeof s);
-
-    console.log("7: SHop Session -- \n", shopSessions[0].isActive());
-
     if (shopSessions?.length > 0) {
       for (const session of shopSessions) {
         console.log("ANSWER: ", session.accessToken);
